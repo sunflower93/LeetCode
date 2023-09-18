e78c6172659d718188d1fd9c271c3da069022161
shaobin <shaobin@zhejianglab.com>
2023-09-18 10:59:10 +0800

    1367 二叉树中的链表

--- src/main/java/leetcode/Solution2023.java
+++ src/main/java/leetcode/Solution2023.java
@@ -5,6 +5,36 @@ import java.util.*;
 
 class Solution2023 {
     /*
+    1367. 二叉树中的链表
+     */
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (head == null) {
+            return true;
+        }
+        if (root == null) {
+            return false;
+        }
+        //先判断当前的节点，如果不对，再看左子树和右子树呗
+        return isSub(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    private boolean isSub(ListNode head, TreeNode node) {
+        //特判：链表走完了，返回true
+        if (head == null) {
+            return true;
+        }
+        //特判：链表没走完，树走完了，这肯定不行，返回false
+        if (node == null) {
+            return false;
+        }
+        //如果值不同，必定不是啊
+        if (head.val != node.val) {
+            return false;
+        }
+        //如果值相同，继续看，左边和右边有一个满足即可
+        return isSub(head.next, node.left) || isSub(head.next, node.right);
+    }
+    /*
     面试题 05.04. 下一个数
      */
     public int[] findClosedNumbers(int num) {
